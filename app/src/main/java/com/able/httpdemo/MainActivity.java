0e17f8d134c703c5887306bcf7073af1ba63949c
zsq7777 <zsqandzyr@hotmail.com>
2018-10-17 23:25:27 +0800

    添加了okhttp的post表单提交与get请求

--- app/src/main/java/com/able/httpdemo/MainActivity.java
+++ app/src/main/java/com/able/httpdemo/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.util.List;
 
 import io.reactivex.Observable;
@@ -15,9 +16,18 @@ import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.FormUrlEncoded;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     //聚合数据
     String appkey = "e201160722b2f9f7e11d45a2e9958037";
     String url="http://v.juhe.cn/todayOnhistory/queryEvent.php?date=1/1&key=e201160722b2f9f7e11d45a2e9958037";
+    String url1="http://v.juhe.cn/todayOnhistory/queryEvent.php";
 
     private static final String TAG = "MainActivity";
     private Handler handler=new Handler();
@@ -35,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button mBtnRetrofit = findViewById(R.id.btn_retrofit_request);
         Button mBtnHttpConnection = findViewById(R.id.btn_httpConnection_request);
+        Button mBtnOkhttp = findViewById(R.id.btn_okhttp);
+        Button mBtnOkhttpPost = findViewById(R.id.btn_okhttp_post);
         final TextView mTvContent = findViewById(R.id.tv_content);
 
         mBtnRetrofit.setOnClickListener(new View.OnClickListener() {
@@ -49,6 +62,79 @@ public class MainActivity extends AppCompatActivity {
                new HttpConnectionThread(handler,mTvContent,url).start();
             }
         });
+        mBtnOkhttp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                okhttpRequest();
+            }
+        });
+        mBtnOkhttpPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                okhttpRequestPost();
+            }
+        });
+
+
+    }
+
+    private void okhttpRequestPost() {
+        //1.创建OkHttpClient对象
+        OkHttpClient okHttpClient=new OkHttpClient();
+
+        //通过new FormBody()调用builder（）创建一个RequestBody,可以用add添加键值对 然后 builder
+        Request.Builder builder = new Request.Builder();
+
+        // 表单提交
+        RequestBody requestBody = new FormBody.Builder().add("key", appkey).add("date", "1/1").build();
+        // 字符串提交
+//        MediaType mediaType=MediaType.parse("application/json; charset=utf-8");
+//        String value="{username:admin;password:admin}";
+//        RequestBody requestBody = RequestBody.create(mediaType, value);
+
+
+        //获取一个request对象
+        final Request request = builder.url(url1).post(requestBody).build();
+
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String s = response.body().string();
+                Log.i(TAG, "onResponse:POST请求 "+s);
+            }
+        });
+
+    }
+
+    private void okhttpRequest() {
+        OkHttpClient okHttpClient=new OkHttpClient();
+
+        //这是request
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.get().url(url).build();
+
+        //call
+        Call call = okHttpClient.newCall(request);
+
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e(TAG, "onFailure: "+e.getMessage() );
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String string = response.body().string();
+                Log.i(TAG, "onResponse: "+string);
+            }
+        });
+
 
     }
 
